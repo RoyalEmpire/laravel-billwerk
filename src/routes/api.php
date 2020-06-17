fe490c42c56911e6959e6d6ef9a61777d848b943
Gazder Bence <bencegazder@gmail.com>
2020-06-17 21:45:25 +0200

    Remove OrderController routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,8 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->middleware(config('laravel-billwerk.api.middleware'))->group(function() {
     Route::post('/billwerk/webhook', 'WebhookController@handle');
 
-    Route::post('/order/preview', 'OrderController@preview');
-    Route::post('/order', 'OrderController@order');
-
     Route::get('/contract/{contractId}/token', 'ContractController@getSelfServiceToken');
 });
